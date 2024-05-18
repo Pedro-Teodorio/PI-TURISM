e9321f438493098eef33b2a5e875f4cf3b62135d
paulo-vinhas <paulo_floriano@hotmail.com>
2024-05-17 23:23:26 -0300

    filters included in products

--- app/helpers/produtos/listar.php
+++ app/helpers/produtos/listar.php
@@ -1,8 +1,9 @@
 <?php
-try {
-    require("../../database/database_config.php");
-    $stmt = $pdo->prepare(
-        "SELECT
+    $nome = filter_input(INPUT_GET, 'nome', FILTER_SANITIZE_URL);
+    if (!empty($nome)) {
+        require("../../database/database_config.php");
+        $query = "
+            SELECT
                 PRODUTO.PRODUTO_ID
               , PRODUTO.PRODUTO_NOME
               , PRODUTO.PRODUTO_DESC
@@ -13,9 +14,56 @@ try {
               , GROUP_CONCAT( PRODUTO_IMAGEM.IMAGEM_URL ) AS IMAGEM_URL
               , GROUP_CONCAT( PRODUTO_IMAGEM.IMAGEM_ORDEM ) AS IMAGEM_ORDEM
               , COALESCE(PRODUTO_ESTOQUE.PRODUTO_QTD,0) AS PRODUTO_QTD
-    
+
             FROM PRODUTO
-    
+
+                INNER JOIN CATEGORIA
+                    USING(CATEGORIA_ID)
+                
+                LEFT JOIN PRODUTO_IMAGEM
+                    USING(PRODUTO_ID)
+                
+                LEFT JOIN PRODUTO_ESTOQUE
+                    USING(PRODUTO_ID)
+                
+            WHERE PRODUTO.PRODUTO_NOME LIKE ?
+            
+            GROUP BY
+                1
+              , 2
+              , 3
+              , 4
+              , 5
+              , 6
+              , 7
+              , 10
+                    
+            ORDER BY
+                PRODUTO.PRODUTO_ID
+              , PRODUTO_IMAGEM.IMAGEM_ORDEM
+        ";
+        $params = array("%$nome%");
+        $stmt = $pdo->prepare($query);
+        $stmt->execute($params);
+        $produto = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $retorna = ["erro" => false, "dados" => $produto];
+    } else {
+        require("../../database/database_config.php");
+        $query = "
+            SELECT
+                PRODUTO.PRODUTO_ID
+              , PRODUTO.PRODUTO_NOME
+              , PRODUTO.PRODUTO_DESC
+              , PRODUTO.PRODUTO_PRECO
+              , PRODUTO.PRODUTO_DESCONTO
+              , PRODUTO.PRODUTO_ATIVO
+              , CATEGORIA.CATEGORIA_NOME
+              , GROUP_CONCAT( PRODUTO_IMAGEM.IMAGEM_URL ) AS IMAGEM_URL
+              , GROUP_CONCAT( PRODUTO_IMAGEM.IMAGEM_ORDEM ) AS IMAGEM_ORDEM
+              , COALESCE(PRODUTO_ESTOQUE.PRODUTO_QTD,0) AS PRODUTO_QTD
+
+            FROM PRODUTO
+
                 INNER JOIN CATEGORIA
                     USING(CATEGORIA_ID)
                 
@@ -37,13 +85,12 @@ try {
                     
             ORDER BY
                 PRODUTO.PRODUTO_ID
-              , PRODUTO_IMAGEM.IMAGEM_ORDEM"
-    );
-    $stmt->execute();
-    $produtos = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    $retorna = ["erro" => false, "dados" => $produtos];
-} catch (PDOException $e) {
-    $retorna = ["erro" => true, "mensagem" => "Erro: " . $e->getMessage()];
-}
-
-echo json_encode($retorna);
+              , PRODUTO_IMAGEM.IMAGEM_ORDEM
+        ";
+        $stmt = $pdo->prepare($query);
+        $stmt->execute();
+        $produto = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $retorna = ["erro" => false, "dados" => $produto];
+    }
+    echo json_encode($retorna);
+?>
